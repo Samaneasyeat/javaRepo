e03e671fc376d4b53819c89605937af97c2556ca
saman <saman@easyeat.ai>
2025-01-10 16:14:39 +0530

    practices question of java with prashant jain classes

---
+++ Pattern2.java
@@ -0,0 +1,23 @@
+
+//Write a code inn java to make this pattern
+// *****
+// ****
+// ***
+// **
+// *
+
+public class Pattern2 {
+    public static void main(String[] args) {
+        for(int i = 5 ;i>=1; i--)
+        {
+            for (int j = 1; j <= i; j++) 
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        
+    }
+    
+}
